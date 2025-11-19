8b4b90e48ae10d77a2534df8b540b12a66211467
thomastokmakov24 <thomast89@nycstudents.net>
2025-11-19 12:32:04 -0500

    dddddd
    dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd

--- app/src/main.js
+++ app/src/main.js
@@ -20,5 +20,18 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
+//meme plan out. find the things. display it it. Select one and not save to a list. Add text. output the meme with the text on the image. Text content. You can use url to do videos and pictures. 
 setupCounter(document.querySelector('#counter'))
+
+bookArray = [
+  { title: "The Joy Luck Club", author: "Amy Tan", year: 2000, read: true  }, //the idea is read is T=> mark as read and to read if False.
+  { title: "The Summer Hikaru Died", author: "Mokumokuren", year: 2023, read: true },
+  { title: "1984", author: "George Orwell", year: 1949, read: false },
+  { title: "DBZ", author: "Akira Toriyama", year: 1989, read: false },
+  { title: "Trials of Apollo", author: "Rick Riordan", year: 2016, read: true },
+];
+
+readbucket = [];
+toreadbucket = [];
+
+
