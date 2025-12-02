fa522b6a9937ddc7ab3f2ec87486b389613539b0
thomastokmakov24 <thomast89@nycstudents.net>
2025-12-02 12:32:08 -0500

    fcdcxccllowporgress
    n

--- app/src/main.js
+++ app/src/main.js
@@ -23,12 +23,12 @@ document.querySelector('#app').innerHTML = `
 //meme plan out. find the things. display it it. Select one and not save to a list. Add text. output the meme with the text on the image. Text content. You can use url to do videos and pictures. 
 setupCounter(document.querySelector('#counter'))
 
-bookArray = [
-  { title: "The Joy Luck Club", author: "Amy Tan", year: 2000, read: true , image: "https://i.pinimg.com/originals/7e/d7/52/7ed7521603385e27b5719b7699b31d47.jpg" }, //the idea is read is T=> mark as read and to read if False.
-  { title: "The Summer Hikaru Died", author: "Mokumokuren", year: 2023, read: true, image: "https://th.bing.com/th/id/OIP.BoehT_tAJalkya1TIoraGAHaKe?w=186&h=263&c=7&r=0&o=7&pid=1.7&rm=3" },
-  { title: "1984", author: "George Orwell", year: 1949, read: false , image: "https://th.bing.com/th/id/OIP.er1VXwv6qs9hs5SAsjJ-XwHaMi?w=186&h=315&c=7&r=0&o=7&pid=1.7&rm=3"},
-  { title: "DBZ", author: "Akira Toriyama", year: 1989, read: false, image: "https://th.bing.com/th/id/OIP.7bEKefwm73Nn_g2eBSuKEQHaLH?w=186&h=279&c=7&r=0&o=7&pid=1.7&rm=3" },
-  { title: "Trials of Apollo", author: "Rick Riordan", year: 2016, read: true, image: "https://th.bing.com/th/id/OIP.j0BqORCtvpx0gO5FYoo-CQHaJ4?w=129&h=180&c=7&r=0&o=7&pid=1.7&rm=3" },
+const bookArray = [
+  { title: "The Joy Luck Club", author: "Amy Tan", year: 2000, read: 1 , image: "https://i.pinimg.com/originals/7e/d7/52/7ed7521603385e27b5719b7699b31d47.jpg" }, //the idea is read is T=> mark as read and to read if False.
+  { title: "The Summer Hikaru Died", author: "Mokumokuren", year: 2023, read: 2, image: "https://th.bing.com/th/id/OIP.BoehT_tAJalkya1TIoraGAHaKe?w=186&h=263&c=7&r=0&o=7&pid=1.7&rm=3" },
+  { title: "1984", author: "George Orwell", year: 1949, read: 3 , image: "https://th.bing.com/th/id/OIP.er1VXwv6qs9hs5SAsjJ-XwHaMi?w=186&h=315&c=7&r=0&o=7&pid=1.7&rm=3"},
+  { title: "DBZ", author: "Akira Toriyama", year: 1989, read: 4, image: "https://th.bing.com/th/id/OIP.7bEKefwm73Nn_g2eBSuKEQHaLH?w=186&h=279&c=7&r=0&o=7&pid=1.7&rm=3" },
+  { title: "Trials of Apollo", author: "Rick Riordan", year: 2016, read: 5, image: "https://th.bing.com/th/id/OIP.j0BqORCtvpx0gO5FYoo-CQHaJ4?w=129&h=180&c=7&r=0&o=7&pid=1.7&rm=3" },
 ];
 //images book covers. Put the two lists on screen preferrableyside by side but top and bottom is fine. Have 
 function inject(cry) {
@@ -43,8 +43,9 @@ function inject(cry) {
         </div>`,
   );
 }
-tobuybucket = [];toreadbucket = [];currentbucket = [];readbucket = [];againbucket = [];
+let tobuybucket = [];let toreadbucket = [];let currentbucket = [];let readbucket = [];let againbucket = [];
 
+//bookArray.forEach(book)
 function cycle(_____) {
   if ((book.read) == 1) {
     tobuybucket.push(book);
@@ -61,17 +62,16 @@ function cycle(_____) {
           else if ((book.read) == 5) {
             againbucket.push(book);
             }
+            else if ((book.read) > 5) {
+              book.read = 1;}  }
 
 
 
-bookArray.forEach(slip)
-function slip(book) {
-  if (book.read) {
-    readbucket.push(book);
-  } else {
-    toreadbucket.push(book);
-  }
-}
+cycle(bookArray.forEach(book));
+
+console.log("Books To Buy:", tobuybucket);
+console.log("Books Currently Reading:", currentbucket);
+console.log("Books To Read Again:", againbucket);
 console.log("Books Read:", readbucket);
 console.log("Books To Read:", toreadbucket);
 
